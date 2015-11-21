30cd290328843137b591a72ab5948792b5558c36
jdibiccari <janel.dibiccari@gmail.com>
2015-11-21 18:02:59 -0500

    begin submissions model

--- fossil_submissions/models.py
+++ fossil_submissions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.gis.geos import Point
 
-# Create your models here.
+def picture_upload_to(self, filename):
+    return image_upload_to(self, re.sub(r'.*\.(\w+)', r'submission_img.\1', filename).lower())
+
+class Submission(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)
+    longitude = models.FloatField(null=True, blank=True)
+    image = models.ImageField(max_length=1000, upload_to=picture_upload_to, null=True, blank=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    @property
+    def location_as_point(self):
+        if self.latitude and self.longitude:
+            return Point(self.longitude, self.latitude)
+        return None
