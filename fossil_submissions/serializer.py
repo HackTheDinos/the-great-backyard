4e181af264fe4fe4bd4f5e891aa679e86e8408d2
jdibiccari <janel.dibiccari@gmail.com>
2015-11-22 10:07:14 -0500

    create users with password through rest framework ui, issue with user_appraisals

--- fossil_submissions/serializer.py
+++ fossil_submissions/serializer.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from fossil_submissions.models import Submission, Appraisal
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, ReadOnlyField
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, ReadOnlyField, HyperlinkedRelatedField
 
 class SubmissionSerializer(ModelSerializer):
     class Meta:
@@ -8,14 +8,20 @@ class SubmissionSerializer(ModelSerializer):
         fields = ('created', 'description', 'latitude', 'image', 'longitude', 'approved', 'reviewed')
 
 class UserSerializer(ModelSerializer):
-    user_appraisals = PrimaryKeyRelatedField(many=True, queryset=Appraisal.objects.all())
+    user_appraisals = HyperlinkedRelatedField(queryset=Appraisal.objects.all(), view_name='appraisal-detail', many=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'user_appraisals',)
+        fields = ('id', 'username', 'user_appraisals', 'password',)
+
+    def create(self, validated_data):
+        user_data = {key: value for key, value in validated_data.items() if key != 'user_appraisals'}
+        user = User.objects.create_user(**user_data)
+        return user
 
 class AppraisalSerializer(ModelSerializer):
     appraiser = ReadOnlyField(source='appraiser.username')
+    submission = HyperlinkedRelatedField(queryset=Submission.objects.all(), view_name='submission-detail')
     class Meta:
         model = Appraisal
         fields = ('appraiser', 'comment', 'submission', 'is_fossil')
