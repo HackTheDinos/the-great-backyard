038a70813c7d6e326f0efd087b569ffe31cfcaa9
jdibiccari <janel.dibiccari@gmail.com>
2015-11-22 09:38:13 -0500

    set appraisal user to logged in user, add permissions to that only logged in users can create appraisals

--- fossil_submissions/serializer.py
+++ fossil_submissions/serializer.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from fossil_submissions.models import Submission, Appraisal
-from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, ReadOnlyField
 
 class SubmissionSerializer(ModelSerializer):
     class Meta:
@@ -15,6 +15,7 @@ class UserSerializer(ModelSerializer):
         fields = ('id', 'username', 'user_appraisals',)
 
 class AppraisalSerializer(ModelSerializer):
+    appraiser = ReadOnlyField(source='appraiser.username')
     class Meta:
         model = Appraisal
         fields = ('appraiser', 'comment', 'submission', 'is_fossil')
