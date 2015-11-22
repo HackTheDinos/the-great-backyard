c26072f8a491ecfa21c0e670dbb594fdbe3c3414
jdibiccari <janel.dibiccari@gmail.com>
2015-11-22 10:57:32 -0500

    add id back to submission serializer fields, add login back to navbar

--- fossil_submissions/serializer.py
+++ fossil_submissions/serializer.py
@@ -5,7 +5,7 @@ from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField, 
 class SubmissionSerializer(ModelSerializer):
     class Meta:
         model = Submission
-        fields = ('created', 'description', 'latitude', 'image', 'longitude', 'approved', 'reviewed')
+        fields = ('id', 'created', 'description', 'latitude', 'image', 'longitude', 'approved', 'reviewed')
         read_only_fields = ('approved', 'reviewed',)
 
 class UserSerializer(ModelSerializer):
